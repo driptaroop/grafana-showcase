4d1417a6c32ec455a6b2b7688d2b902ed761576e
Driptaroop Das <driptaroop.das@dkbcodefactory.com>
2022-02-07 23:05:40 +0100

    feat(init): init commit

---
+++ src/main/kotlin/com/example/temposhowcase/TempoShowcaseApplication.kt
@@ -0,0 +1,11 @@
+package com.example.temposhowcase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TempoShowcaseApplication
+
+fun main(args: Array<String>) {
+    runApplication<TempoShowcaseApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/temposhowcase/TempoShowcaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.temposhowcase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TempoShowcaseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
